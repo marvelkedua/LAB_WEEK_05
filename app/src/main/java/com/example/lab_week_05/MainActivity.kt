a85e1d9b09c5d9fdb86c82e325a7d1999edbbb02
marvelkn <marvel_kevin12@yahoo.com>
2025-09-26 08:18:17 +0700

    Commit No. 1 - Retrofit Setup and Fetch API JSON Response

--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -5,44 +5,52 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.TextView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import com.example.lab_week_05.api.CatApiService
 
 class MainActivity : AppCompatActivity() {
 
-    private val retrofit by lazy{
+    private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://api.thecatapi.com/v1/")
             .addConverterFactory(ScalarsConverterFactory.create())
             .build()
     }
 
-    private val catApiService by lazy {
+    private val catApiService: CatApiService by lazy {
         retrofit.create(CatApiService::class.java)
     }
 
-    private val apiResponseView: TextView by lazy{
+    private val apiResponseView: TextView by lazy {
         findViewById(R.id.api_response)
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getCatImageResponse()
     }
+
     private fun getCatImageResponse() {
         val call = catApiService.searchImages(1, "full")
-        call.enqueue(object: Callback<String> {
+        call.enqueue(object : Callback<String> {
             override fun onFailure(call: Call<String>, t: Throwable) {
                 Log.e(MAIN_ACTIVITY, "Failed to get response", t)
             }
-            override fun onResponse(call: Call<String>, response:
-            Response<String>) {
-                if(response.isSuccessful){
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
                     apiResponseView.text = response.body()
-                }
-                else{
-                    Log.e(MAIN_ACTIVITY, "Failed to get response\n" +
-                            response.errorBody()?.string().orEmpty()
+                } else {
+                    Log.e(
+                        MAIN_ACTIVITY, "Failed to get response\n" +
+                                response.errorBody()?.string().orEmpty()
                     )
                 }
             }
